47df110ecca08015b47b1e273600065a9b6062e9
stephane-lima <ggabriellopess99999@gmail.com>
2024-03-01 14:37:27 -0100

    Project 1 Part 1

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,6 @@ router.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+router.use("/contacts", require("./contacts"))
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const app = express();
+const mongodb = require("./data/database")
 const indexRoute = require("./routes");
 
-app.use("/", indexRoute);
+const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`app listening on port ${port}`);
-});
+app.use("/", indexRoute);
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`database is listening and node is running on port ${port}`);
+        });
+    }
+})
